9ca29e96698f9a012ca6ed267ddc2cfa834c9914
sbjustin <justin@simplybinary.com>
2013-08-01 23:03:07 -0400

    trying to figure out the event loop now

--- monopoly.js
+++ monopoly.js
@@ -2,15 +2,24 @@
 var monopoly = {
 
   numberOfPlayers: 2,
-  setupGame:       document.getElementById("setup_game"),
-  startGame:       document.getElementById("start_game"),
-  gamePanel:       document.getElementById("game_panel"),
-  leftPane:        document.getElementById("left_pane"),
-  rightPane:       document.getElementById("right_pane"),
-  board:           document.getElementById("board"),
-  dice:            document.getElementById("dice"),
-  addPlayer:       document.getElementById('add_player'),
-  playerEntryForm: document.getElementById('player_entry_form'),
+  startingMoney: 1500,
+  phase: 'playing', //diceroll
+  gameOver: false,
+  playersArray: [],
+  setupGame:              document.getElementById("setup_game"),
+  startGame:              document.getElementById("start_game"),
+  gamePanel:              document.getElementById("game_panel"),
+  leftPane:               document.getElementById("left_pane"),
+  rightPane:              document.getElementById("right_pane"),
+  board:                  document.getElementById("board"),
+  dice:                   document.getElementById("dice"),
+  addPlayer:              document.getElementById("add_player"),
+  //playerEntryForm:        document.getElementById('player_entry_form'),
+  playerEntryFormList:    document.getElementById("player_entry_form_list"),
+  playerEntries:          document.getElementsByName("playername"),
+  outputLog:              document.getElementById("output_log"),
+  nextTurnButton:         document.getElementById("next_turn_button"),
+  endGameButton:          document.getElementById("end_game_button"),
 
   init: function(){
     var me = this;
@@ -23,15 +32,23 @@ var monopoly = {
       me.rightPane.style.display = 'block';
       me.board.style.display = 'block';
       me.dice.style.display = 'block';
+
+      me.setupPlayers();
+
+      me.playGame();
     });
 
-    this.dice.addEventListener("click", function(){
-      alert('Roll Dice!');
+    me.endGameButton.addEventListener("click", function(){
+      me.gameOver = true;
     });
 
     this.addPlayer.addEventListener("click", function(){
       me.numberOfPlayers = me.numberOfPlayers + 1;
-      me.playerEntryForm.innerHTML += "Player" + me.numberOfPlayers + "'s Name: <input type='text' name='player" + me.numberOfPlayers + "name'><br>";
+      var newLI = document.createElement("LI");
+      me.playerEntryFormList.appendChild(newLI);
+      newLI.innerHTML = "Player" + me.numberOfPlayers + "'s Name: <input type='text' name='playername' class = 'player_entry_form_field'>";
+      //One Liner to do it
+      //me.playerEntryFormList.appendChild(document.createElement("LI")).innerHTML = "Player" + me.numberOfPlayers + "'s Name: <input type='text' name='player" + me.numberOfPlayers + "name' class = 'player_entry_form_field'>";
       if (me.numberOfPlayers == 8){
         me.addPlayer.style.visibility = 'hidden';
       };
@@ -42,15 +59,58 @@ var monopoly = {
     this.init();
   }, 
 
-  Player: function(){
+  playGame: function(){
+    //while (monopoly.gameOver != true){
+      for (var i = 0; i < monopoly.numberOfPlayers; i++){
+        monopoly.writeToOutputLog(monopoly.playersArray[i].name + ", it's your turn"); 
+        monopoly.phase = 'diceroll';
+        monopoly.allowRollDice();
+        //monopoly.phase = 'move';
+      }
+      
+      // check - is there a winner?
+      monopoly.gameOver = true;
+      monopoly.writeToOutputLog('Game Over');
+    //};
+  },
+
+  allowRollDice: function(){
+      this.dice.addEventListener("click", function(){
+        if (monopoly.phase == 'diceroll'){
+          var diceRoll = Math.floor(Math.random() * (12 - 2 + 1) + 2);
+          monopoly.writeToOutputLog('You rolled a ' + Math.floor(Math.random() * (12 - 2 + 1) + 2) + '!');
+          monopoly.phase == 'complete';
+        };
+      });
+  },
+
+  setupPlayers: function(){ 
+    monopoly.writeToOutputLog(this.numberOfPlayers + " players are joining");
+    for (var i = 0; i < this.numberOfPlayers; ++i) {
+      this.playersArray[i] = new this.Player(this.playerEntries[i].value);  
+      monopoly.writeToOutputLog("Player " + this.playersArray[i].name + " has joined the game.");
+    };
+    
+  },
+
+  writeToOutputLog: function(notice){
+    var newLI = document.createElement("LI");
+    monopoly.outputLog.insertBefore(newLI, monopoly.outputLog.childNodes[0]);
+    newLI.innerHTML = notice;
+    if (monopoly.outputLog.childNodes.length > 20){
+      monopoly.outputLog.removeChild(monopoly.outputLog.lastChild);
+    };
+  },
+
+  Player: function(name){
     this.name = name;
-    this.cash = 0;
+    this.cash = monopoly.startingMoney;
     this.position = 0; //Position on board 0 -> 39
   },
 
   Property: function(){
     this.name = name;
-    this.cost = 0;
+    this.cost = monopoly.startingMoney;
     this.owner = '';
   }
 
