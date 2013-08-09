7b5cd9176fc0c0cfde9d540d2013cd6275cbf347
sbjustin <justin@simplybinary.com>
2013-08-08 21:18:13 -0400

    removed autostart

--- monopoly.js
+++ monopoly.js
@@ -69,8 +69,8 @@ var monopoly = {
     var me = this;
 
     //remove this stuff for real game
-    this.playerEntries[0].value = "Justin";
-    this.playerEntries[1].value = "Cat"
+    // this.playerEntries[0].value = "Justin";
+    // this.playerEntries[1].value = "Cat"
     ////////////////////////////////////////////
 
     me.defProperties();
@@ -91,7 +91,7 @@ var monopoly = {
       me.playGame();
     });
     //remove this stuff for real game
-     monopoly.startGame.click();
+     // monopoly.startGame.click();
      //monopoly.properties[1].showModal();
     ////////////////////////////////////////////    
 
